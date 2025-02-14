8078664057235aa15b146b2687fcd5b5c6da4c35
aashikur <mdaashikur@gmail.com>
2025-02-14 21:35:23 +0600

    a

---
+++ 01.js
@@ -0,0 +1,9 @@
+const bottle = {
+    brand : 'apple',
+    price : 45,
+    color : 'white',
+    isClean: false,
+    sub : [12,34,56,78,44],
+    presure : function(){console.log("I'm getting Killed!!")}
+};
+
