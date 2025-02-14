574a35540b91e3d27637afebf7d8be56157a7948
aashikur <mdaashikur@gmail.com>
2025-02-14 21:45:38 +0600

    first Name

--- 01.js
+++ 01.js
@@ -4,6 +4,30 @@ const bottle = {
     color : 'white',
     isClean: false,
     sub : [12,34,56,78,44],
-    presure : function(){console.log("I'm getting Killed!!")}
+    presure : function(){console.log("I'm getting Killed!!")},
+    'big job': ['job 1', 'job 2','job 3']
 };
 
+console.log(bottle);
+console.log(bottle.price);
+
+bottle.price = 999; // change the inner Value
+
+console.log(bottle['price']);
+console.log(bottle['big job']);
+
+let res = bottle.sub;
+console.log(res);
+
+// How to Access 
+/*
+    1. Bracket notation
+    2. third bracket diye access
+
+    bottle.price
+    bottle['price']
+*/
+
+// KeyName ==========
+const keyName = 'price';
+console.log(bottle[keyName]);
